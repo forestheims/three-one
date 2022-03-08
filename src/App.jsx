0221160860a380f65f9bccf02311693671e5944e
forestheims <forestheims@gmail.com>
2022-03-08 10:10:12 -0800

    tinkering with the camera and renderer. Writing notes as comments to learn what each piece of code does.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { animate } from './three/three';
 
 export default function App() {
-  return <>{animate()}</>;
+  animate();
+  return <></>;
 }
--- src/three/three.js
+++ src/three/three.js
@@ -1,29 +1,50 @@
 import * as THREE from 'three';
 
+// Scene
 const scene = new THREE.Scene();
+
+// Camera
+// PerspectiveCamera is most like seeing from human eyes
 const camera = new THREE.PerspectiveCamera(
-  100,
+  // field of view (ammount of world visible) out 360 degrees
+  75,
+  // aspect ratio
   window.innerWidth / window.innerHeight,
-  0.5,
+  // view frustum
+  // frustum -the portion of a cone or pyramid which remains
+  // after its upper part has been cut off by a plane parallel
+  // to its base, or which is intercepted between two such planes.
+  0.1,
+  // distance away objects can be seen
   1000
 );
 
+// renders the 3D objects onto a DOM element
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// an object has geometry that must be defined
+const geometry = new THREE.IcosahedronGeometry();
+// material is the wrapping paper
+// custom shaders can be created using WebGL
+const material = new THREE.MeshBasicMaterial({
+  color: 0x9966d8,
+  wireframe: true,
+});
+// Meshing creates the defined material wrapped geometry
+const icosahedron = new THREE.Mesh(geometry, material);
+scene.add(icosahedron);
 
 camera.position.z = 5;
 
 export function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  icosahedron.rotation.x += 0.001;
+  icosahedron.rotation.y += 0.001;
+  icosahedron.rotation.z += 0.001;
 
   renderer.render(scene, camera);
 }
