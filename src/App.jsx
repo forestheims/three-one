dd65ebbb1b6df055b5e0cecbb9adc01c528270ca
forestheims <forestheims@gmail.com>
2022-03-08 17:05:22 -0800

    raycaster added. mousemove event listener added. Stars and icosahedron now turn transparent when hovered over with the mouse.

--- src/App.jsx
+++ src/App.jsx
@@ -2,66 +2,35 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 export default function App() {
-  // Scene
   const scene = new THREE.Scene();
 
-  // Camera
-  // PerspectiveCamera is most like seeing from human eyes
   const camera = new THREE.PerspectiveCamera(
-    // field of view (ammount of world visible) out 360 degrees
     75,
-    // aspect ratio
     window.innerWidth / window.innerHeight,
-    // view frustum
-    // frustum -the portion of a cone or pyramid which remains
-    // after its upper part has been cut off by a plane parallel
-    // to its base, or which is intercepted between two such planes.
     0.1,
-    // distance away objects can be seen
     1000
   );
 
-  // renders the 3D objects onto a DOM element
   const renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.setPixelRatio(window.devicePixelRatio);
   document.body.appendChild(renderer.domElement);
 
-  // an object has geometry that must be defined
   const geometry = new THREE.IcosahedronGeometry();
-  // material is the wrapping paper
-  // custom shaders can be created using WebGL
-  // BasicMaterial does not interact with lighting
-  // StandardMaterial does interact with lighting
   const material = new THREE.MeshStandardMaterial({
     color: 0x9966bf,
   });
-  // Meshing creates the defined material wrapped geometry
   const icosahedron = new THREE.Mesh(geometry, material);
-  // adding the shape to the scene;
   scene.add(icosahedron);
-  camera.position.z = 20;
 
-  const pointLight = new THREE.PointLight(0xffffff);
-  pointLight.position.set(5, 5, 5);
-  // const lightHelper = new THREE.PointLightHelper(pointLight);
-  // scene.add(lightHelper);
+  camera.position.z = 5;
 
-  const ambientLight = new THREE.AmbientLight(0x9966bf);
-
-  scene.add(pointLight, ambientLight);
-
-  // const gridHelper = new THREE.GridHelper(200, 50);
-  // scene.add(gridHelper);
-
-  // listens to dom elements from the mouse, and updates the camera
   const controls = new OrbitControls(camera, renderer.domElement);
 
   function addStar() {
     const starGeometry = new THREE.SphereGeometry(0.33, 24, 24);
     const starMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
     const star = new THREE.Mesh(starGeometry, starMaterial);
-
     const [x, y, z] = Array(3)
       .fill()
       .map(() => THREE.MathUtils.randFloatSpread(100));
@@ -69,23 +38,63 @@ export default function App() {
     scene.add(star);
   }
 
-  Array(333).fill().forEach(addStar);
+  Array(123).fill().forEach(addStar);
 
-  // const backgroundTexture = new THREE.TextureLoader().load(pic);
-  // scene.background = backgroundTexture;
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(5, 5, 5);
+  const ambientLight = new THREE.AmbientLight(0x222222);
+  scene.add(pointLight, ambientLight);
+
+  const pointer = new THREE.Vector2();
+  const raycaster = new THREE.Raycaster();
+
+  function onPointerMove(event) {
+    pointer.x = (event.clientX / window.innerWidth) * 2 - 1;
+    pointer.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  }
+
+  function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  function hoverThing() {
+    raycaster.setFromCamera(pointer, camera);
+    const intersects = raycaster.intersectObjects(scene.children);
+    for (let i = 0; i < intersects.length; i++) {
+      intersects[i].object.material.transparent = true;
+      intersects[i].object.material.opacity = 0.5;
+    }
+  }
+
+  function resetMaterials() {
+    for (let i = 0; i < scene.children.length; i++) {
+      if (scene.children[i].material) {
+        scene.children[i].material.opacity = 1.0;
+      }
+    }
+  }
 
   function animate() {
     requestAnimationFrame(animate);
 
-    icosahedron.rotation.x += 0.001;
-    icosahedron.rotation.y += 0.005;
-    icosahedron.rotation.z += 0.001;
+    icosahedron.rotation.x += 0.005;
+    icosahedron.rotation.y += 0.01;
+    icosahedron.rotation.z += 0.005;
 
     controls.update();
-
+    resetMaterials();
+    hoverThing();
     renderer.render(scene, camera);
   }
 
+  window.addEventListener('resize', onWindowResize);
+  window.addEventListener('mousemove', onPointerMove, false);
+
   animate();
+  console.log();
+
   return <></>;
 }
